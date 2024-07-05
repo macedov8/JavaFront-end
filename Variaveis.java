80d244ecfefc39a3c7026048e1bb276a2a1fbd1e
macedov8 <lucasmacedov8@gmail.com>
2024-07-05 14:20:11 -0300

    Add files via upload

---
+++ Variaveis.java
@@ -0,0 +1,29 @@
+public class Variaveis{ 
+    public static void main(String[] args) throws Exception { 
+        int varInt = 2; // declaração de variável varInt, do tipo int, com valor recebido 2
+        System.out.println("Variável do tipo int (inteiro): " + varInt);
+
+        Integer varInteger = 3; // declaração de variável varInteger, do tipo Integer, com valor recebido 3
+        System.out.println("Variável do tipo Interger: " + varInteger);
+
+        String varString = "texto"; // declaração de variável varString, do tipo String, com valor recebido texto
+        System.out.println("Variável do tipo String (texto): " + varString);
+
+        boolean varBoolean = true; // declaração de variável varBoolean, do tipo boolean, com valor recebido true
+        System.out.println("Variável do tipo boolean (verdadeiro ou falso): " + varBoolean);
+
+        byte varByte = 1; // declaração de variável varByte, do tipo byte, com valor recebido 1
+        System.out.println("Variável do tipo byte: " + varByte);
+
+        char varChar = 'r'; // declaração de variável varChar, do tipo char, com valor recebido r
+        System.out.println("Variável do tipo char(caracteres): " + varChar);
+
+        float varFloat = 19.99f; // declaração de variável varFloat, do tipo float, com valor recebido 19.99f
+        System.out.println("Variável do tipo float (flutuantes): " + varFloat);
+
+        double varDouble = 0.263568954; // declaração de variável varDouble, do tipo double, com valor recebido 0.263568954
+        System.out.println("Variável do tipo double (flutuante de precisão dupla): " + varDouble); 
+
+
+    }
+}
